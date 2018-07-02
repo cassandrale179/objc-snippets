d6c04b36425ef4b93180e974bab6b2dc5337f116
Minh Le <lenguyetminh1998@gmail.com>
2018-07-02 10:23:52 -0700

    Create dynamicbinding.m

---
+++ dynamicbinding.m
@@ -0,0 +1,4 @@
+/* Definition: Dynamic binding is determining the method to invoke at runtime instead of at compile time.  
+Dynamic binding enables polymorphism. If Rectangle & Square both has a printArea method, 
+the expression [anObject printArea] is determined at runtime.
+*/
